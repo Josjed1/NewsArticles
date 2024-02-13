6b1e16149bbf7a459078aab493ba0cc3f353776b
Jojed1 <jocaley@siue.com>
2024-02-12 18:59:09 -0600

    Pushing final

--- main.py
+++ main.py
@@ -5,37 +5,28 @@ def main():
     # Get URL from user input
     url = input("Enter the URL: ")
     
-    #If you want to grab urls others ways, change line 6 to a commit and then unhashtag the other options below
-    #url = 'full_url'
+    # If you want to grab URLs in other ways, change line 6 to a comment and then uncomment other options below
+    # url = 'full_url'
+    # url = 'name_of_file'
     
-    #url = name of file
     response = requests.get(url)
     
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         
-        article_body = soup.find('div', class_='article-body')
+        # Find all paragraphs in the HTML
+        paragraphs = soup.find_all('p')
+            
+        if paragraphs:
+            article_text = '\n'.join([paragraph.get_text() for paragraph in paragraphs])
 
-        if article_body:
-            article_text = article_body.get_text()    
-
-            # Split the text into words
-            words = article_text.split()
-
-            # Define the maximum number of words per line
-            max_words_per_line = 20
-
-            # Write to file with a new line after every 30 words
-            with open("output_onepiece.txt", 'a') as f:
-                for i in range(0, len(words), max_words_per_line):
-                    line = ' '.join(words[i:i+max_words_per_line])
-                    f.write(line + '\n')
-
+            # Write to file
+            with open("test.txt", 'a') as f:
+                f.write(article_text)
         else:
-            print("Couldn't find the article body element.")
+            print("Couldn't find any paragraphs in the article.")
     else:
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
 if __name__ == "__main__":
-    
     main()
